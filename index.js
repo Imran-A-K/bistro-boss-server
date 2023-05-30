dbb0e6c2b760a3dbcd73f992767a586b632b277b
Imran-A-K <imran.k.professional@gmail.com>
2023-05-30 19:48:47 +0600

    added delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express()
@@ -58,6 +58,14 @@ async function run() {
       const result = await cartCollection.insertOne(item)
       res.send(result);
     })
+
+    app.delete('/carts/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query)
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
