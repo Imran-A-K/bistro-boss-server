e46e6d68db4ddef5ef9a582a7b851df96676c477
Imran-A-K <imran.k.professional@gmail.com>
2023-05-30 18:16:57 +0600

    added carts get api with query method with email

--- index.js
+++ index.js
@@ -42,10 +42,19 @@ async function run() {
         res.send(result)
     })
 
-    // cart collection
+    // cart collection apis
+    app.get('/carts', async(req, res) => {
+      const email = req.query?.email;
+      if(!email){
+        return res.send([]);
+      }
+      const query = { email : email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
     app.post('/carts', async(req,res) => {
       const item = req.body;
-      console.log(item)
+      // console.log(item)
       const result = await cartCollection.insertOne(item)
       res.send(result);
     })
