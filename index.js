05ff66dacc8fe49d2d5c1e484d2385cccd8e7ed8
Imran-A-K <imran.k.professional@gmail.com>
2023-06-02 15:58:17 +0600

    added verify jwt function

--- index.js
+++ index.js
@@ -25,6 +25,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middleware jwt function it goes into the middle of jwt get api
+
+  const verifyJWT = async(req,res,next) =>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    // bearer token  token is literally it means the token value you get from the user by authorization in headers
+    const token = authorization.split(' ')[1];
+
+    // token verify main function from jwt docs
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if(err){
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+      }
+      // if the jwt token is verified
+      req.decoded = decoded;
+      next() // passing to the next function
+    })
+  }
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -37,7 +58,13 @@ async function run() {
 
     // jwt api
 
-    
+    app.post('/jwt', (req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({ token });
+    })
 
     // user related api's
 
@@ -56,6 +83,20 @@ async function run() {
         return res.send({ message: 'user already exists' })
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+    // api to check if the user is an admin or not
+    // verify jwt is the first security layer
+    app.get('/users/admin/:email',verifyJWT, async(req,res) => {
+      const email = req.params.email;
+      // adding second layer of security checking theemail address that is calling 
+      // the api with the token is the same the email we get from decoding the token
+      if(req.decoded.email !== email){
+        return res.send({ admin : false })
+      }
+      const query = { email : email}
+      const user = await usersCollection.findOne(query)
+      const result = { admin: user?.role === 'admin' }
       res.send(result);
     })
 
@@ -85,10 +126,15 @@ async function run() {
     })
 
     // cart collection apis
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', verifyJWT, async(req, res) => {
       const email = req.query?.email;
       if(!email){
         return res.send([]);
+      }
+      const decodedEmail = req.decoded.email // getting the decoded email from the response of jwt
+      if(email !== decodedEmail){
+        //adding extra level of security
+        return res.status(403).send({error: true, message: 'forbidden access'})
       }
       const query = { email : email};
       const result = await cartCollection.find(query).toArray();
